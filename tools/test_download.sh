599681b99981b60a725dc5c880a7f78ad81d4daa
Michael Hauspie <michael.hauspie@lifl.fr>
2012-07-09 16:23:13 +0200

    Checks the http return code

--- tools/test_download.sh
+++ tools/test_download.sh
@@ -3,8 +3,10 @@
 # $1: ip
 # $2: target
 # $3: get url
+# $4: expected http code (200 if omitted)
+# return true if the expected http code is returned by smews
 
-CURL="curl -g --connect-timeout 2 --max-time 3"
+CURL="curl -s -S -g --connect-timeout 2 --max-time 3 -o /dev/null -w %{http_code}"
 
 ip_addr=$1
 if echo $ip_addr | grep -q ':'
@@ -12,9 +14,22 @@ then
     ip_addr="[$ip_addr]" # ipv6 must be in square brackets in url
 fi
 
-
-if ! $CURL  http://$ip_addr/$3 >& /dev/null
+HTTP_CODE=`$CURL http://$ip_addr/$3`
+echo $?
+if [ $? -ne 0 ]
 then
     exit 1
 fi
-exit 0
+
+REQUEST_CODE=$4
+
+if [ -z "$REQUEST_CODE" ]
+then
+    REQUEST_CODE=200
+fi
+if [ $HTTP_CODE = $REQUEST_CODE ]
+then
+    exit 0
+fi
+
+exit 1
