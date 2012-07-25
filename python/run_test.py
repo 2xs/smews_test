69088072a2882bd58007ed91d8c62885757cfa3b
Michael Hauspie <michael.hauspie@lifl.fr>
2012-07-25 23:36:33 +0200

    Adds a test report class

--- python/run_test.py
+++ python/run_test.py
@@ -3,11 +3,33 @@
 import sys, os, stat, subprocess
 
 
+class Test:
+    __tests = []
+    def begin(target, what):
+        Test.__tests.append({"target": target, "what": what, "success": False})
+    def success():
+        Test.__tests[len(Test.__tests)-1]["success"] = True
+        sys.stdout.write(".")
+        sys.stdout.flush()
+    def fail():
+        sys.stdout.write("X")
+        sys.stdout.flush()
+    def report():
+        print("")
+        success = 0
+        for test in Test.__tests:
+            if not test["success"]:
+                sys.stderr.write("FAIL({}): {}\n".format(test["target"], test["what"]))
+            else:
+                success = success + 1
+        sys.stdout.write("{}/{} tests passed ({:.2f} %)\n".format(success, len(Test.__tests), success / len(Test.__tests) * 100))
+#####################################################
+
 def chdir(path):
     current_path = os.getcwd()
     os.chdir(path)
     return current_path
-######################################################    
+#####################################################    
 
 def get_target_list():
     global smews_folder
@@ -19,7 +41,7 @@ def get_target_list():
         if not stat.S_ISDIR(mode):
             targets.remove(target)
     return targets
-######################################################    
+#####################################################    
 
 def execute_command(args):
     process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -30,7 +52,7 @@ def execute_command(args):
             break
     if process.returncode:
         raise subprocess.CalledProcessError(process.returncode, args)
-######################################################
+#####################################################
 
 def perform_build(build_options):
     global smews_folder
@@ -46,24 +68,26 @@ def perform_build(build_options):
         chdir(test_path)
         raise
     chdir(test_path)
-########################################################
+#####################################################
 
 
 
 if len(sys.argv) < 2:
     sys.stderr.write("Usage: {0} <smews_folder>\n".format(sys.argv[0]))
+    sys.exit(1)
 else:
     smews_folder = sys.argv[1]
 
 targets=get_target_list()
+ips = ["192.168.100.200", "fc23::2"]
 
-current_target=""
 for target in targets:
-    current_target = target
-    try:
-        print("Performing build for target {}".format(target))
-        perform_build({"ipaddr": "192.168.100.200", "target": target})
-    except OSError as e:
-        sys.stderr.write("Build of target {} failed: {}\n".format(current_target, e))
-    except subprocess.CalledProcessError as e:
-        sys.stderr.write("Build of target {} failed: {}\n".format(current_target, e))
+    for ip in ips:
+        Test.begin(target, "build({})".format(ip))
+        try:
+            perform_build({"ipaddr": ip, "target": target})
+            Test.success()
+        except:
+            Test.fail()
+
+Test.report()
