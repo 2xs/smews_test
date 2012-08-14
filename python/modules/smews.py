8546d5ec3b9e74165e58f3f780d724488fef8054
Michael Hauspie <michael.hauspie@lifl.fr>
2012-08-14 11:20:05 +0200

    Adds program and run test

--- python/modules/smews.py
+++ python/modules/smews.py
@@ -1,7 +1,30 @@
 import os
 from . import system
 
+class SmewsError(Exception):
+    pass
+
+class SmewsProgramError(SmewsError):
+    def __init__(self, message):
+        self.message = message
+
+class SmewsBuildError(SmewsError):
+    def __init__(self, message):
+        self.message = message
+
+class SmewsRunError(SmewsError):
+    def __init__(self, message):
+        self.message = message
+
 folder = "."
+tools_folder = "."
+
+def build_options_to_string(build_options):
+    str = ""
+    for (option,value) in build_options.items():
+        str = str + " {}={}".format(option,value)
+    return str
+    
 
 def build(build_options):
     global folder
@@ -13,6 +36,8 @@ def build(build_options):
         for (option,value) in build_options.items():
             args.append("{}={}".format(option,value))
         system.execute(args)
+    except system.ExecutionError as e:
+        raise SmewsBuildError(e.message)
     finally:
         system.chdir(test_path)
 #####################################################
@@ -32,3 +57,25 @@ def get_disable_options():
 def get_apps():
     global folder
     return system.get_subfolder_list(os.path.join(folder, "apps"))
+
+def program(target):
+    global folder
+    global tools_folder
+    program_script = os.path.join(os.path.join(tools_folder,target),"program")
+    args = [program_script, folder]
+    try:
+        system.execute(args)
+    except system.ExecutionError as e:
+        raise SmewsProgramError(e.message)
+
+
+def run(target):
+    global folder
+    global tools_folder
+    run_script = os.path.join(os.path.join(tools_folder,target),"run")
+    args = [run_script, folder]
+    try:
+        system.execute(args)
+    except system.ExecutionError as e:
+        raise SmewsRunError(e.message)
+    
--- python/run_test.py
+++ python/run_test.py
@@ -7,32 +7,56 @@ TEST_SUITES_FOLDER="test_suites"
 
 
 def test_build(test_suite, build_options):
-    what = "build(test_suite: {} -- ip: {} -- ".format(test_suite, build_options["ipaddr"])
-    if (build_options["disable"] == ""):
-        what = what + "all options)"
-    else:
-        what = what + "disabled options: {})".format(build_options["disable"])
+    what = "build(test_suite: {} -- {})".format(test_suite, smews.build_options_to_string(build_options))
     test.begin(build_options["target"], what)
     try:
         smews.build(build_options)
         test.success()
-    except system.ExecutionError as e:
+    except smews.SmewsError as e:
         test.fail(e.message)
 #####################################################
 
 
+def test_program(test_suite, build_options):
+    what = "program(test_suite: {} -- {})".format(test_suite, smews.build_options_to_string(build_options))
+    test.begin(build_options["target"], what)
+    try:
+        smews.program(build_options["target"])
+        test.success()
+    except smews.SmewsError as e:
+        test.fail(e.message)
+
+def test_run(test_suite, build_options):
+    what = "run(test_suite: {} -- {})".format(test_suite, smews.build_options_to_string(build_options))
+    test.begin(build_options["target"], what)
+    try:
+        smews.run(build_options["target"])
+        test.success()
+    except smews.SmewsError as e:
+        test.fail(e.message)
+
+def do_tests(test_suite, build_options):
+    pass
+
+def test_kill(test_suite, build_options):
+    pass
+
 if len(sys.argv) < 2:
     sys.stderr.write("Usage: {0} <smews_folder> [target1 ... targetN]\n".format(sys.argv[0]))
     sys.exit(1)
-else:
-    smews.folder = sys.argv[1]
-    targets_to_test = sys.argv[2:]
 
-script_folder = sys.path[0]
+# Set needed folder to absolute paths
+script_folder = os.path.abspath(sys.path[0])
+smews.folder = os.path.abspath(sys.argv[1])
+smews.tools_folder = os.path.join(script_folder, "tools")
+# update the PATH environment so that all run scripts have a direct access to the tools scripts
+os.environ["PATH"] = os.environ["PATH"] + ":" + smews.tools_folder
+
+
+targets_to_test = sys.argv[2:]
 test_suites.folder = os.path.join(script_folder, "test_suites")
 
 ips = ["192.168.100.200", "fc23::2"]
-
 
 try:
     test_suites_list = test_suites.get_list()
@@ -49,9 +73,18 @@ try:
                 build_options["ipaddr"] = ip
                 for disable in disable_list:
                     build_options["disable"] = ",".join(disable)
+                    # Build smews
                     test_build(test_suite, build_options)
-                #execute_test_suite(test_suite, build_options)
+                    # Flash to device
+                    test_program(test_suite, build_options)
+                    # Run smews
+                    test_run(test_suite, build_options)
+                    # Executes tests
+                    do_tests(test_suite, build_options)
+                    # Kill
+                    test_kill(test_suite, build_options)
+
 except KeyboardInterrupt:
     sys.stderr.write("\nAbording tests\n")
-
-test.report(True)
+finally:
+    test.report(True)
