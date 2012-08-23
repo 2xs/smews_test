645b6cc0f56f0ff4c8fcb15371b6094ab254d0a9
Michael Hauspie <michael.hauspie@lifl.fr>
2012-08-23 14:54:42 +0200

    Refactors some code in global filter

--- test_suites/filter.py
+++ test_suites/filter.py
@@ -5,6 +5,11 @@
 def icmpv6_needed():
     return ['mbed_ethernet']
 
+def discard_targets():
+    """List of targets that must not be tested yet"""
+    return ['WSN430wireless', 'GBA', 'WSN430', 'MicaZwireless', 'cygwin','WSN430','skeleton']
+
+
 def is_v6(ip):
     return ':' in ip
 
@@ -12,14 +17,15 @@ def validate_build(build_options):
     # This function check if the build corresponds to a valid configuration
     # that has to be tested
     
+    # skeleton is not a real target
+    if build_options["target"] in discard_targets():
+        return False
+
     # icmpv6 NEEDS GPIP, if disabled, the build should not be performed
     if "icmpv6" in build_options["apps"]:
         return not "general_purpose_ip_handler" in build_options["disable"]
     return True
 
-def discard_targets():
-    """List of targets that must not be tested yet"""
-    return ['WSN430wireless', 'GBA', 'WSN430', 'MicaZwireless', 'cygwin','WSN430','skeleton']
 
 def filter(build_options):
     # This is the global filter for smews tests.
@@ -28,9 +34,6 @@ def filter(build_options):
     # If a filter returns true, the test can be performed, otherwise, it is an incompatible test
     # so it will not be performed
 
-    # skeleton is not a real target
-    if build_options["target"] in discard_targets():
-        return False
     
     # First, check if we are in ipv6, and, if so, check if the target needs the icmpv6 app to work
     if is_v6(build_options["ipaddr"]):
