92c5f1e51ffd10709d6ba5a71050e140123804a6
Michaël Hauspie <michael.hauspie@lifl.fr>
2013-03-20 15:18:55 +0100

    Adds a parameter to test_download to checkout the downloaded file

--- tools/test_download.sh
+++ tools/test_download.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-
 # $1: ip
 # $2: get url
 # $3: expected http code (200 if omitted)
+# $4: path to a file that should be equivalent to the downloaded file
 # return true if the expected http code is returned by smews
 
-CURL="curl -s -S -g --connect-timeout 3 --retry 2 --max-time 3 -o /dev/null -w %{http_code}"
+out_file="/dev/null"
+if [ $# -ge 4 ]
+then
+    out_file=`mktemp /tmp/tmp.XXXXXXXX`
+fi
+
+CURL="curl -s -S -g --connect-timeout 3 --retry 2 --max-time 3 -o $out_file -w %{http_code}"
 
 ip_addr=$1
 if echo $ip_addr | grep -q ':'
@@ -28,6 +34,18 @@ then
 fi
 if [ $HTTP_CODE = $REQUEST_CODE ]
 then
+    if ! [ $out_file = "/dev/null" ]
+    then
+	if file $out_file | grep -q "gzip"
+	then
+	    gunzip -c $out_file | diff - $4 > /dev/null
+	else
+	    diff $out_file $4 > /dev/null
+	fi
+	exit_code=$?
+	rm -f $out_file
+	exit $exit_code
+    fi
     exit 0
 fi
 
